2bb13c98741a6677d1dbc1dde36432cf0a01207d
Mikhail Utkin <mihut@rambler.ru>
2022-09-22 18:09:15 +0500

    refactored starting script

--- start.py
+++ start.py
@@ -1,20 +1,2 @@
-from flask import Flask, render_template, redirect, url_for, request
-from collections import namedtuple
-
-
-app = Flask(__name__)
-
-message = namedtuple('Message', 'text tag')
-messages = []
-
-@app.route("/", methods=['GET'])
-def index():
-
-    return render_template('index.html')
-
-@app.route('/add_message')
-def add_message():
-    import myTestFile
-    # return f"SUMMARY {myTestFile.runZTool()}"
-    return render_template('summary.html', report=myTestFile.runZTool())
-
+import main
+main.runZTool()
